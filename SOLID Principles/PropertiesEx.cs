220e38971a23b02e03ba1673ffe37b8f3ebd0a8a
akash <akash@DESKTOP-B1PDELG>
2026-05-23 15:05:49 +0530

    Properties Example

---
+++ SOLID Principles/PropertiesEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles
+{
+    class Student
+    {
+       private String name;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+    internal class PropertiesEx
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "John Doe";
+            Console.WriteLine("Student Name: " + student.Name);
+        }
+    }
+}
--- SOLID Principles/SRP_Constructor.cs
+++ SOLID Principles/SRP_Constructor.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SOLID_Principles
-{
-    class Student
-    {
-        int Sid;
-        string Sname;
-        public Student()
-        {
-            Sid = 1;
-            Sname = "John Doe";
-            Console.WriteLine("Student ID: " + Sid);
-            Console.WriteLine("Student Name: " + Sname);
-            Console.WriteLine("Student details initialized.");
-        }
-        public void DisplayStudentDetails()
-        {
-            Console.WriteLine("Student ID: " + Sid);
-            Console.WriteLine("Student Name: " + Sname);
-        }
-    }
-    internal class SRP_Constructor
-    {
-        static void Main(string[] args)
-        {
-            Student student = new Student();
-            student.DisplayStudentDetails();
-        }
-    }
-}
+//namespace SOLID_Principles
+//{
+//    class Student
+//    {
+//        int Sid;
+//        string Sname;
+//        public Student()
+//        {
+//            Sid = 1;
+//            Sname = "John Doe";
+//            Console.WriteLine("Student ID: " + Sid);
+//            Console.WriteLine("Student Name: " + Sname);
+//            Console.WriteLine("Student details initialized.");
+//        }
+//        public void DisplayStudentDetails()
+//        {
+//            Console.WriteLine("Student ID: " + Sid);
+//            Console.WriteLine("Student Name: " + Sname);
+//        }
+//    }
+//    internal class SRP_Constructor
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student student = new Student();
+//            student.DisplayStudentDetails();
+//        }
+//    }
+//}
